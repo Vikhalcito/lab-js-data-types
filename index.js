ec100cfeb1c1170d834fae6c74b2343db6394bf4
Victor Vikhal <victor.vikhal@gmail.com>
2025-02-17 15:41:02 +0100

    Solved lab

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
 
-let tongueTwister = s1 + " " + s2 + " " + s3 + " " +  s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4 ;
-
+// let tongueTwister = s1 + " " + s2 + " " + s3 + " " +  s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4 ;
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
 console.log(tongueTwister);
 
 // Concatenate the string variables into one new string
@@ -25,8 +25,10 @@ console.log(tongueTwister);
 const part1 = "java";
 const part2 = "script";
 
-let result = part1.slice(0, part1.length - 1) + part1.slice(part1.length-1).toUpperCase() + part2.slice(0, part2.length - 1) + part2.slice(part2.length-1).toUpperCase()
-
+// let result = part1.slice(0, part1.length - 1) + part1.slice(part1.length-1).toUpperCase() + part2.slice(0, part2.length - 1) + part2.slice(part2.length-1).toUpperCase()
+let newPart1 = `${part1.slice(0, part1.length -1)}${part1.slice(part1.length-1).toUpperCase()}`
+let newPart2 = `${part2.slice(0, part2.length -1)}${part2.slice(part2.length-1).toUpperCase()}`
+let result = `${newPart1}${newPart2}`
 console.log(result)
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
